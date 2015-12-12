fec929e929fc3ab8ac95d07858bd738502331394
Cynthia Chen <checy16@gmail.com>
2015-12-11 16:32:30 -0800

    "Set up MongoDB pipeline to insert scraped data into database"

--- coffee_shop/coffee_shop/pipelines.py
+++ coffee_shop/coffee_shop/pipelines.py
@@ -6,27 +6,43 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 import pymongo
-from scrapy.conf import settings
+from scrapy import settings
 from scrapy.exceptions import DropItem
-from scrapy import log
 
 class MongoDBPipeline(object):
-    def __init__(self):
-      connection = pymongo.MongoClient(
-        settings['MONGODB_SERVER'],
-        settings['MONGODB_PORT']
-      )
-      db = connection[settings['MONGODB_DB']]
-      self.collection = db[settings['MONGODB_COLLECTION']]
 
-    def process_item(self, item, spider):
-        valid = True
-        for data in item:
-          if not data:
-            valid = False
-            raise DropItem('Missing {0}!'.format(data))
-        if valid:
-          self.collection.insert(dict(item))
-          log.msg('Entry added to MongoDB database!',
-            level=log.DEBUG, spider=spider)
-        return item
+  collection_name = 'coffee_shops'
+
+  def __init__(self, mongo_uri, mongo_db):
+    self.mongo_uri = mongo_uri
+    self.mongo_db = mongo_db
+    # connection = pymongo.MongoClient(
+    #   settings['MONGODB_SERVER'],
+    #   settings['MONGODB_PORT']
+    # )
+    # db = connection[settings['MONGODB_DB']]
+    # self.collection = db[settings['MONGODB_COLLECTION']]
+  
+  @classmethod
+
+  def from_crawler(cls, crawler):
+    return cls(
+      mongo_uri=crawler.settings.get('MONGO_URI'),
+      mongo_db=crawler.settings.get('MONGODB_DATABASE', 'items')
+    )
+  def open_spider(self, spider):
+    self.client = pymongo.MongoClient(self.mongo_uri)
+    self.db = self.client[self.mongo_db]
+
+  def close_spider(self, spider):
+    self.client.close()
+
+  def process_item(self, item, spider):
+    valid = True
+    for data in item:
+      if not data:
+        valid = False
+        raise DropItem('Missing {0}!'.format(data))
+    if valid:
+      self.db[self.collection_name].insert(dict(item))
+      return item
