3098f49294da2d10a50ae4b1768b952ee2c7297f
Cynthia Chen <checy16@gmail.com>
2016-01-31 16:37:41 -0800

    Add loading and error handling

--- client/app.js
+++ client/app.js
@@ -7,44 +7,70 @@ app.controller('ShopsController', function ($scope, ShopFactory) {
   $scope.shop = {};
   $scope.flag = false;
   $scope.errorFlag = false;
+  $scope.error_text = '';
+  $scope.loadFlag = false;
+
+  $scope.resetFlags = function() {
+    $scope.loadFlag = false;
+    $scope.errorFlag = false;
+    $scope.flag = false;
+  };
 
   $scope.getShopData = function() {
-    var name = $scope.name.split(' ').join('-').toLowerCase();
-    if (name !== 'starbucks' || name !== 'peets') {
-      if (name === 'blue-bottle') {
-        name = 'blue-bottle-coffee-co';
-      } else if (name === 'ritual') {
-        name = 'ritual-coffee-roasters';
-      } else if (name === 'sightglass') {
-        name = 'sightglass-coffee';
-      } else if (name === 'contraband') {
-        name = 'contraband-coffee-bar';
-      } else if (name === 'reveille') {
-        name = 'reveille-coffee-co.'
-      } else if (name === 'four-barrel') {
-        name = 'four-barrel-coffee';
-      } else if (name === 'trouble-coffee') {
-        name = 'trouble-coffee-company';
-      } else if (name === 'the-station') {
-        name = 'the-station-sf';
-      } else if (name === 'flywheel') {
-        name = 'flywheel-coffee-roasters';
+    $scope.loadFlag = true;
+    $scope.errorFlag = false;
+    $scope.flag = false;
+    if ($scope.name) {
+      var name = $scope.name.split(' ').join('-').toLowerCase();
+      if (name !== 'starbucks' || name !== 'peets' || name !== '') {
+        if (name === 'blue-bottle') {
+          name = 'blue-bottle-coffee-co';
+        } else if (name === 'ritual') {
+          name = 'ritual-coffee-roasters';
+        } else if (name === 'sightglass') {
+          name = 'sightglass-coffee';
+        } else if (name === 'contraband') {
+          name = 'contraband-coffee-bar';
+        } else if (name === 'reveille') {
+          name = 'reveille-coffee-co.'
+        } else if (name === 'four-barrel') {
+          name = 'four-barrel-coffee';
+        } else if (name === 'trouble-coffee') {
+          name = 'trouble-coffee-company';
+        } else if (name === 'the-station') {
+          name = 'the-station-sf';
+        } else if (name === 'flywheel') {
+          name = 'flywheel-coffee-roasters';
+        } else if (name === 'philz') {
+          name = 'philz-coffee';
+        }
+        $scope.name = '';
+        $scope.flag = false;
+
+        ShopFactory.getData(name)
+          .then(function(resp) {
+            $scope.loadFlag = false;
+            if (typeof resp === "string") {
+              $scope.errorFlag = true;
+              $scope.error_text = resp;
+            } else {
+              $scope.shop = resp;
+              $scope.shop.name = resp.name.split('-').join(' ');
+              $scope.flag = true;
+            }
+          })
+          .catch(function(err) {
+            $scope.errorFlag = true;
+            $scope.error_text = err;
+            console.log('Error in getting shop data', err);
+          });
       }
-      $scope.name = '';
-      $scope.flag = false;
-      ShopFactory.getData(name)
-        .then(function(resp) {
-          $scope.shop = resp;
-          $scope.shop.name = resp.name.split('-').join(' ');
-          $scope.flag = true;
-          console.log('shop data', $scope.shop);
-        })
-        .catch(function(err) {
-          $scope.errorFlag = true;
-          $scope.error = 'Sorry, we cannot find that coffee shop... Maybe it\'s too hipster...';
-          console.log('Error in getting shop data', err);
-        });
-    }
+    } else {
+      $scope.loadFlag = false;
+      $scope.errorFlag = true;
+      $scope.error_text = 'Please enter a valid coffee shop name';
+      console.log('error', $scope.error_text);
+    };
   };
 });
 
